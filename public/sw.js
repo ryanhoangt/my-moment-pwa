3aeb65e76a9137e87b69e0d4721373189c1d4acf
Hoang Tran <descience.thh10@gmail.com>
2022-12-23 14:54:36 +0700

    support caching for offline access using service worker

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,63 @@
+var CACHE_STATIC_NAME = "static-v4";
+var CACHE_DYNAMIC_NAME = "dynamic-v2";
+
 self.addEventListener("install", function (event) {
-  console.log("[SW] Installing service worker...", event);
+  console.log("[SW] Installing service worker...");
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME).then(function (cache) {
+      console.log("[SW] Precaching App Shell...");
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/promise.js",
+        "/src/js/fetch.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+      ]);
+    })
+  );
 });
 
 self.addEventListener("activate", function (event) {
-  console.log("[SW] Activating service worker...", event);
+  console.log("[SW] Activating service worker...");
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map((keyElem) => {
+          if (keyElem !== CACHE_STATIC_NAME && keyElem !== CACHE_DYNAMIC_NAME) {
+            console.log("[SW] Removing old cache:", keyElem);
+            return caches.delete(keyElem);
+          }
+        })
+      );
+    })
+  );
   return self.clients.claim();
 });
 
 self.addEventListener("fetch", function (event) {
-  console.log("[SW] Fetching something...");
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        // find a match in the cache
+        return response;
+      } else {
+        return fetch(event.request)
+          .then(function (res) {
+            return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+              cache.put(event.request.url, res.clone()); // not consume the original response
+              return res; // return response back after putting it to cache
+            });
+          })
+          .catch(function (err) {});
+      }
+    })
+  );
 });
